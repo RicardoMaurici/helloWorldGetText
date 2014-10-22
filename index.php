1ed356c8945e6c1bbcc4224d84a808c4d67ac48d
Ricardo Maurici <ricardo.maurici@hotmail.com>
2014-10-22 10:35:49 -0200

    hello world em pt_PT e pt_BR

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ 
+require_once('config.php');
+require_once('i18n.php');
+ 
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title><?php echo __('Hello World!'); ?></title>
+  </head>
+  <body>
+    <h1><?php _e('Hello World!'); ?></h1>
+  </body>
+</html>
